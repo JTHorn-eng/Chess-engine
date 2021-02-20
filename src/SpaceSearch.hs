c2841ba0eccad074aec0273495d505292947c2bc
JTHorn-eng <jamesh2173@gmail.com>
2021-02-20 07:16:49 +0000

    Edited negamax routine

--- src/SpaceSearch.hs
+++ src/SpaceSearch.hs
@@ -14,23 +14,30 @@ generateMoves :: B.Board -> States
 generateMoves board =
 
 orderMoves :: States -> States
-orderMoves states =
+orderMoves states = 
 
 
 
 --explore child nodes 
 --value, childNodes(states), alpha, beta, depth
-negamaxRoutine :: Int -> States -> Int -> Int -> Int -> Int
-negamaxRoutine val childNodes a b depth = 
+--output tuple = (value, new alpha)
+negamaxRoutine :: Int -> States -> Int -> Int -> Int -> Int -> (Int, Int)
+negamaxRoutine val childNodes a b depth color = 
+    (newVal, newAlpha)
+    where
+    newAlpha = maximum (a, newVal)
+    newVal = maximum out
+    out = map (\n -> negate (negamax n (depth - 1) (negate b) (negate a) (negate color))) childNodes
+    
 
 
 --node, depth, alpha, beta, color
-negamax :: B.Board -> Int -> Int -> Int -> Int -> Int
+negamax :: B.Board -> Int -> Int -> Int -> Int -> (Int, Int)
 negamax node depth a b color
-    | depth == 0 = color * (findHeuristic node)
+    | depth == 0 = (color * (findHeuristic node), a)
     | otherwise = newOptVal
     where
-    newOptVal = negamaxRoutine minInt childNodes a b depth
+    newOptVal = negamaxRoutine minInt childNodes a b depth color
     childNodes = orderMoves $ generateMoves node
     minInt = minBound :: Int
     
