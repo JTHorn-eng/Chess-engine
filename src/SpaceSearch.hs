459fe172c1dfe7a47c5f229b03bbfd6c223e0b00
JTHorn-eng <jamesh2173@gmail.com>
2021-02-20 07:31:47 +0000

    Fixed passing new alpha value

--- src/SpaceSearch.hs
+++ src/SpaceSearch.hs
@@ -17,25 +17,24 @@ orderMoves :: States -> States
 orderMoves states = 
 
 
+newAlpha :: Int -> Int -> Int
+newAlpha a newVal = maximum (a, newVal)
 
 --explore child nodes 
 --value, childNodes(states), alpha, beta, depth
 --output tuple = (value, new alpha)
-negamaxRoutine :: Int -> States -> Int -> Int -> Int -> Int -> (Int, Int)
-negamaxRoutine val childNodes a b depth color = 
-    (newVal, newAlpha)
+negamaxRoutine :: Int -> States -> Int -> Int -> Int -> Int -> Int
+negamaxRoutine val childNodes a b depth color = newVal
     where
-    newAlpha = maximum (a, newVal)
     newVal = maximum out
     out = map (\n -> negate (negamax n (depth - 1) (negate b) (negate a) (negate color))) childNodes
     
 
-
 --node, depth, alpha, beta, color
-negamax :: B.Board -> Int -> Int -> Int -> Int -> (Int, Int)
+negamax :: B.Board -> Int -> Int -> Int -> Int -> Int
 negamax node depth a b color
-    | depth == 0 = (color * (findHeuristic node), a)
-    | otherwise = newOptVal
+    | depth == 0 = color * findHeuristic node
+    | otherwise = negamaxRoutine minInt childNodes (newAlpha a newOptVal) b depth color
     where
     newOptVal = negamaxRoutine minInt childNodes a b depth color
     childNodes = orderMoves $ generateMoves node
